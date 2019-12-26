b2f77dee7268165788c49217aca4d6ac72c024fd
Gan Hong Jun <hongjun87@gmail.com>
2019-12-26 18:19:31 +0800

    Add Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,49 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <v-btn>a</v-btn>
-      <HelloWorld />
-    </v-content>
-  </v-app>
+  <v-app>    
+    <v-container>      
+      <v-row class="text-center">
+        <v-col cols="6">
+          <h2>You</h2>
+        </v-col>
+        <v-col cols="6">
+          <h2>Monster</h2>
+        </v-col>
+      </v-row>    
+      <v-row class="text-center">
+        <v-col>{{ health }}</v-col>
+        <v-col>{{ monsterHealth }}</v-col>
+      </v-row>
+      <v-row class="text-center">
+        <v-col>
+          <v-btn id="attack" >Attack</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="primary" id="specialAttack">Special Attack</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="success" id="heal">Heal</v-btn>
+        </v-col>
+        <v-col>
+          <v-btn color="error" id="surrender">Surrender</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>  
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
-export default {
-  name: "App",
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
-};
+  export default {
+    name: "App",  
+    components: {
+      
+    },
+    data: () => ({
+      health: 100,
+      monsterHealth: 100
+    })
+  };
 </script>
+
+<style>
+
+</style>
